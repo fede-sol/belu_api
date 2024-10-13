be4614628e88ac3f1834d7fee40381b60adf1253
Federico Solanes <fedesolanes1@gmail.com>
2024-10-13 01:32:17 -0300

    Refactor URL patterns to include reservations endpoints

--- belu_api/urls.py
+++ belu_api/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('belu_auth.urls')),
+    path('', include('hotels.urls')),
+    path('reservations/', include('reservations.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- reservations/urls.py
+++ reservations/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('reservations', views.UserReservationListCreateView.as_view(), name='user-reservation-list-create'),
-    path('reservations/<int:pk>', views.ReservationDetailView.as_view(), name='reservation-detail'),
+    path('', views.UserReservationListCreateView.as_view(), name='user-reservation-list-create'),
+    path('<int:pk>', views.ReservationDetailView.as_view(), name='reservation-detail'),
 ]
 
